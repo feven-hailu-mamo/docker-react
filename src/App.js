950722ff2949357e62480a1d880c8e93f0b3c5a9
Feven <feven.h.mamo@gmail.com>
2021-03-20 14:50:09 -0500

    testing changes from local

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing change reflection
         </a>
       </header>
     </div>
